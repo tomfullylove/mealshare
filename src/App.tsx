fca9a28038eabe5120c8fd1af6eb0a173206444a
Tom Fullylove <t.fullylove@hotmail.co.uk>
2020-06-06 12:56:12 +0100

    Add redux to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+
+import store from './redux/store';
+
 import TabNavigation from './navigation/TabNavigation';
 
 const App: React.FC = () => {
-  return <TabNavigation />;
+  return (
+    <Provider store={store}>
+      <TabNavigation />
+    </Provider>
+  );
 };
 
 export default App;
