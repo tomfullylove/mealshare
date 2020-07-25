7da258b8daadc614b80c160454ac0b1fa33d272a
Tom Fullylove <tomfullylove@Fullyloves-MacBook-Pro.local>
2020-07-25 16:31:27 +0100

    Bump dependencies and add icon fonts

--- android/app/src/main/java/com/mealshare/MainApplication.java
+++ android/app/src/main/java/com/mealshare/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 import io.sentry.RNSentryPackage;
 import com.facebook.react.ReactInstanceManager;
