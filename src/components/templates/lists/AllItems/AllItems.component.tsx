ab4405caea0076b96b1fd25490130d56a2b2c62e
Tom Fullylove <t.fullylove@hotmail.co.uk>
2020-07-28 13:36:49 +0100

    Refactor all items component to use meals from redux store

--- src/components/templates/lists/AllItems/AllItems.component.tsx
+++ src/components/templates/lists/AllItems/AllItems.component.tsx
@@ -3,44 +3,23 @@ import ListItem from '../../../organisms/ListItem';
 
 import {Container} from './assets/styles';
 
-const AllItems: React.FC = () => {
+interface Props {
+  meals: any;
+}
+
+const AllItems: React.FC<Props> = ({meals}) => {
   return (
     <Container horizontal showsHorizontalScrollIndicator={false}>
-      <ListItem
-        name="Beef Ramen"
-        portions={4}
-        rating={4}
-        price={4.5}
-        distance={1}
-      />
-      <ListItem
-        name="Fish Pie"
-        portions={2}
-        rating={4}
-        price={4}
-        distance={1}
-      />
-      <ListItem
-        name="Vegan Salad"
-        portions={2}
-        rating={3}
-        price={3}
-        distance={3}
-      />
-      <ListItem
-        name="Chicken Fajitas"
-        portions={6}
-        rating={3}
-        price={2.5}
-        distance={2}
-      />
-      <ListItem
-        name="Veg Lasanga"
-        portions={5}
-        rating={4}
-        price={4}
-        distance={1}
-      />
+      {meals.map((meal) => (
+        <ListItem
+          key={meal.uuid}
+          name={meal.title}
+          portions={meal.portions}
+          rating={meal.rating}
+          price={meal.price}
+          distance={meal.price}
+        />
+      ))}
     </Container>
   );
 };
